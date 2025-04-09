4ea859578b5958224073f61ff5bc169c84f3268a
Jasper Orenius <ad4018@student.jamk.fi>
2025-04-09 10:41:11 +0300

    Aloitettu logiikkaa tulolähteiden lisäämiseksi

---
+++ Tax_Liability_Forecast_App/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tax_Liability_Forecast_App.Models
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+    }
+}
