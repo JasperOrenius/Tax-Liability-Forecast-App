6182b85ceba58afd1c5d8d706b31815c645de760
Migis54 <mikkelsson2006@gmailc.om>
2025-04-09 12:20:32 +0300

    osa bindaukset tehty

--- Tax_Liability_Forecast_App/ViewModels/DashboardViewModel.cs
+++ Tax_Liability_Forecast_App/ViewModels/DashboardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiveCharts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace Tax_Liability_Forecast_App.ViewModels
 {
     public class DashboardViewModel : BaseViewModel
     {
+        public ChartValues<decimal> IncomevsExpenseSeries { get; set; } = new ChartValues<decimal>();
+        public ChartValues<decimal> TimeForecast { get; set; } = new ChartValues<decimal>();
+
+        private decimal income;
+        private decimal expense;
+
     }
 }
