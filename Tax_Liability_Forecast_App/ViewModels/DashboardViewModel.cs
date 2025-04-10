2a4f38d8c9adc4c930583b99b437dd4da46b58d9
Migis54 <mikkelsson2006@gmailc.om>
2025-04-10 12:05:40 +0300

    bugi fixaus

--- Tax_Liability_Forecast_App/ViewModels/DashboardViewModel.cs
+++ Tax_Liability_Forecast_App/ViewModels/DashboardViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,58 @@ namespace Tax_Liability_Forecast_App.ViewModels
 {
     public class DashboardViewModel : BaseViewModel
     {
-        public ChartValues<decimal> IncomevsExpenseSeries { get; set; } = new ChartValues<decimal>();
+        public SeriesCollection IncomevsExpenseSeries { get; set; } = new SeriesCollection();
         public ChartValues<decimal> TimeForecast { get; set; } = new ChartValues<decimal>();
 
         private decimal income;
         private decimal expense;
+        private string incomeText;
+        private string expenseText;
+        private string estimatedTaxText;
+        private string deadlineText;
+        public string IncomeText
+        {
+            get => incomeText;
+            set
+            {
+                incomeText = value;
+                OnPropertyChanged(nameof(IncomeText));
+            }
+        }
+        public string ExpenseText
+        {
+            get => expenseText;
+            set
+            {
+                expenseText = value;
+                OnPropertyChanged(nameof(ExpenseText));
+            }
+        }
+        public string EstimatedTaxText
+        {
+            get => estimatedTaxText;
+            set
+            {
+                estimatedTaxText = value;
+                OnPropertyChanged(nameof(EstimatedTaxText));
+            }
+        }
+        public string DeadlineText
+        {
+            get => deadlineText;
+            set
+            {
+                deadlineText = value;
+                OnPropertyChanged(nameof(DeadlineText));
+            }
+        }
+        
+        public DashboardViewModel() {
+            IncomeText = "0";
+            ExpenseText = "0";
+            EstimatedTaxText = "0";
+            DeadlineText = "0";
+        }
 
     }
 }
