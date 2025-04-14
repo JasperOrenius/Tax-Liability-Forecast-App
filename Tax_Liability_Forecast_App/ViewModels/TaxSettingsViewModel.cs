9909f9d2e5591e332c72348ca2a54c10911eb629
mikko Reini <AD4016@student.jamk.fi>
2025-04-14 10:21:12 +0300

    tax settings view jatkettu ettenpäin

--- Tax_Liability_Forecast_App/ViewModels/TaxSettingsViewModel.cs
+++ Tax_Liability_Forecast_App/ViewModels/TaxSettingsViewModel.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Tax_Liability_Forecast_App.Commands;
 
 namespace Tax_Liability_Forecast_App.ViewModels
 {
     public class TaxSettingsViewModel : BaseViewModel
     {
+        public ICommand SaveCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public List<string> taxDeductionList = new List<string>
+        {
+            "Työmatkakulut",
+            "Kotitalousvähennys",
+            "Tulonhankkimisvähennys",
+            "Asunnon ja työpaikan välinen matka",
+            "Opintolainavähennys",
+            "Ansiotulovähennys",
+            "Elatusvelvollisuusvähennys",
+            "Jäsenmaksut",
+            "Lahjoitusvähennys"
+        };
+        public List<string> TaxDeductionList
+        {
+            get => taxDeductionList;
+            set
+            {
+                taxDeductionList = value;
+                OnPropertyChanged(nameof(TaxDeductionList));
+            }
+        }
+
+        public TaxSettingsViewModel()
+        {
+            SaveCommand = new RelayCommand(TaxSettingSave);
+            DeleteCommand = new RelayCommand(TaxSettingDelete);
+        }
+        async Task TaxSettingSave()
+        {
+            
+
+
+        }
+        async Task TaxSettingDelete()
+        {
+
+        }
     }
 }
