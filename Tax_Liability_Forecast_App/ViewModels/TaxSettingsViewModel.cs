22eaef1db6fc979eba0323ce21046a5d6e68fd4d
Migis54 <mikkelsson2006@gmailc.om>
2025-04-24 12:02:52 +0300

    tax settings ui valmis

--- Tax_Liability_Forecast_App/ViewModels/TaxSettingsViewModel.cs
+++ Tax_Liability_Forecast_App/ViewModels/TaxSettingsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Tax_Liability_Forecast_App.Commands;
+using Tax_Liability_Forecast_App.Models;
+
 
 namespace Tax_Liability_Forecast_App.ViewModels
 {
@@ -14,6 +17,43 @@ namespace Tax_Liability_Forecast_App.ViewModels
     {
         public ICommand SaveCommand { get; }
         public ICommand DeleteCommand { get; }
+        public ObservableCollection<TaxBracket> TaxBrackets { get; set; }
+        public ObservableCollection<DeductionType> DeductionTypes { get; set; }
+        public ObservableCollection<TaxDeadline> TaxDeadlines { get; set; }
+
+        // Commands
+        public ICommand AddTaxBracketCommand { get; }
+        public ICommand EditTaxBracketCommand { get; }
+        public ICommand DeleteTaxBracketCommand { get; }
+
+        public ICommand AddDeductionCommand { get; }
+        public ICommand EditDeductionCommand { get; }
+        public ICommand DeleteDeductionCommand { get; }
+
+        public ICommand AddDeadlineCommand { get; }
+        public ICommand EditDeadlineCommand { get; }
+        public ICommand DeleteDeadlineCommand { get; }
+
+        public class TaxBracket
+        {
+            public decimal From { get; set; }
+            public decimal To { get; set; }
+            public decimal Rate { get; set; }
+        }
+
+        public class DeductionType
+        {
+            public string Name { get; set; }
+            public decimal Amount { get; set; }
+            public bool IsDeductible { get; set; }
+        }
+
+        public class TaxDeadline
+        {
+            public string Period { get; set; }
+            public DateTime DueDate { get; set; }
+        }
+
 
         public List<string> taxDeductionList = new List<string>
         {
