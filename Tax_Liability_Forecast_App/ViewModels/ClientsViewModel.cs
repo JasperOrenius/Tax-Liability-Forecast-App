fa31168b7300d0276fe26af3dc826fd647ace222
Joel Rentola <AD4013@student.jamk.fi>
2025-05-07 10:11:56 +0300

    Reworked the UI for client addition and addded error handling

--- Tax_Liability_Forecast_App/ViewModels/ClientsViewModel.cs
+++ Tax_Liability_Forecast_App/ViewModels/ClientsViewModel.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -44,11 +45,42 @@ namespace Tax_Liability_Forecast_App.ViewModels
                 OnPropertyChanged(nameof(Clients));
             }
         }
+        private string clientNameInput;
+        private string clientEmailInput;
+        private string clientPhoneNumInput;
+
+        public string ClientNameInput
+        {
+            get => clientNameInput;
+            set
+            {
+                clientNameInput = value;
+                OnPropertyChanged(nameof(ClientNameInput));
+            }
+        }
+        public string ClientEmailInput
+        {
+            get => clientEmailInput;
+            set
+            {
+                clientEmailInput = value;
+                OnPropertyChanged(nameof(ClientEmailInput));
+            }
+        }
+        public string ClientPhoneNumInput
+        {
+            get => clientPhoneNumInput;
+            set
+            {
+                clientPhoneNumInput = value;
+                OnPropertyChanged(nameof(ClientPhoneNumInput));
+            }
+        }
 
         public ClientsViewModel(IDatabaseService databaseService)
         {
             this.databaseService = databaseService;
-            AddBtnClick = new ClientCommand(AddBtnClickFunc);
+            AddBtnClick = new RelayCommand(AddBtnClickFunc);
             RemoveBtnClick = new ClientCommand(RemoveBtnClickFunc);
             EditBtnClick = new RelayCommand(EditBtnClickFunc);
             EditBtnText = "Edit";
@@ -58,23 +90,53 @@ namespace Tax_Liability_Forecast_App.ViewModels
         private async void FetchTable()
         {
             Clients.Clear();
-            var result = await databaseService.FetchClientTable();
-            Clients = new ObservableCollection<Client>(result);
+            try
+            {
+                var result = await databaseService.FetchClientTable();
+                Clients = new ObservableCollection<Client>(result);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
-        async Task AddBtnClickFunc(Client client)
+        async Task AddBtnClickFunc()
         {
+            Regex EmailValidation = new Regex("^\\S+@\\S+\\.\\S+$");
+            Regex PhoneNumValidation = new Regex("^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$");
+            if (string.IsNullOrEmpty(ClientNameInput) || string.IsNullOrEmpty(ClientEmailInput) || string.IsNullOrEmpty(ClientPhoneNumInput))//ClientNameInput == string.Empty || ClientEmailInput == string.Empty || ClientPhoneNumInput == string.Empty)
+            {
+                MessageBox.Show("Please fill all fields!!!");
+                return;
+            }
+            else if (!EmailValidation.IsMatch(ClientEmailInput))
+            {
+                MessageBox.Show("Email address not valid!!!");
+                return;
+            }
+            else if (!PhoneNumValidation.IsMatch(ClientPhoneNumInput))
+            {
+                MessageBox.Show("Invalid phone number!!!");
+                return;
+            }
 
-            //Client client = new Client() {Name = client1.Name, Email = client1.Email, PhoneNum = client1.PhoneNum };
+            Client client = new Client() {Name = ClientNameInput, Email = ClientEmailInput, PhoneNum = ClientPhoneNumInput};
             client.Id = Guid.NewGuid();
             var result = MessageBox.Show($"Are you sure that you want to add a new client with these values; \nName: {client.Name} \nEmail: {client.Email} \nPhone number: {client.PhoneNum}", "Note!!!", MessageBoxButton.YesNo);
             if (result == MessageBoxResult.Yes)
             {
                 await databaseService.AddClient(client);
+                ClientNameInput = string.Empty;
+                ClientEmailInput = string.Empty;
+                ClientPhoneNumInput = string.Empty;
                 FetchTable();
             }
             else if (result == MessageBoxResult.No)
             {
+                ClientNameInput = string.Empty;
+                ClientEmailInput = string.Empty;
+                ClientPhoneNumInput = string.Empty;
                 FetchTable();
                 return;
             }
@@ -82,7 +144,14 @@ namespace Tax_Liability_Forecast_App.ViewModels
 
         async Task RemoveBtnClickFunc(Client client)
         {
-            await databaseService.RemoveClient(client);
+            try
+            {
+                await databaseService.RemoveClient(client);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
             FetchTable();
         }
 
